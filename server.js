e06167d9219fbc43f1f1f4c9c5ceebc263e04a19
Nosayaba Oliha <42349769+nosaoliha@users.noreply.github.com>
2022-06-16 13:13:03 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,42 +1,50 @@
-//this setup empty javascript object to act as endpoint for all routes
-	projectData = {}
+//this setup empty javascript object to act as endpoint for all routes.
+let projectData = {};
 
-//this requires Express to run server and routes
-let express = require('express')
-let bodyParser = require('body-parser')
-let cors = require('cors')
-let fetch = require('node-fetch')
+//this requires Express to run server and routes.
+let http = require('http');
+let express = require('express');
+let bodyParser = require('body-parser');
+let morgan = require('morgan');
+let cors = require('cors');
 
 //this start up an instance of app
-let app = express()
+let app = express();
 
 /* Middleware*/
 //this configures express to use body-parser as middle-ware.
-	app.use(bodyParser.urlencoded({extended: false}))
-	app.use(bodyParser.json())
+	app.use(morgan('dev'));
+	app.use(bodyParser.urlencoded({ extended: false }));
+	app.use(bodyParser.json());
 
 //this cors for cross origin allowance
-	app.use(cors())
+	app.use(cors());
 
 //this initializes the main project folder
-	app.use(express.static('website'))
+	app.use(express.static('website'));
 
 //this is to GET Route to retrieve projectData
-	app.get('/api/projectdata', (req, res) => {
-	res.status(200).send(projectData)
-})
+	app.get('/projectData', (req, res) => {  
+	res.status(200).send(projectData);
+});
 
 //this is to POST Route to store date, temp and user input in projectData
-	app.post('/api/projectdata', (req, res) => {
-let {date, temp, content} = req.body
-	projectData[date] = {
-    temp,
-    content,
-}
-	res.status(201).send()
+	app.post('/projectData', (req, res) => {
+	projectData = {
+    date: req.body.date,
+    temp: req.body.temp,
+    content: req.body.content
+};
+	console.log(projectData);
+	res.status(200).send({
+    sucess: true,
+    message: "Data saved successfully",
+    data: projectData
+});
 })
 
 //this is to setup Server
-	app.listen(8080, () => {
-	console.log('Running server on port 8080 with CORS enabled.')
-})
+let port = 8000;
+let hostname = 'localhost';
+let server = http.createServer(app);
+	server.listen(port, hostname, () => console.log(`Server running on http://${hostname}:${port}`));
